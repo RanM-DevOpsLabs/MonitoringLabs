680595cf3a41fbd103dfece3bf23475a7a43b8f1
RanMarkovich <markovich.ran@gmail.com>
2025-06-28 23:49:43 +0300

    prefoncifgured dashboard

---
+++ Prometheus/grafana/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    grafana = {
+      source  = "grafana/grafana"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "grafana" {
+  url  = "http://localhost:3000"
+  auth = "admin:admin"
+}
+
+
+resource "grafana_dashboard" "node_exporter_full_dashboard" {
+  config_json = file("${path.module}/1860_rev41.json")
+}
