e778e0363148e74c6b66246b1577479c9860ad36
RanMarkovich <markovich.ran@gmail.com>
2025-06-29 09:27:28 +0300

    src

--- Prometheus/grafana/main.tf
+++ Prometheus/grafana/main.tf
@@ -12,7 +12,7 @@ provider "grafana" {
   auth = "admin:admin"
 }
 
-
+# source: https://grafana.com/grafana/dashboards
 resource "grafana_dashboard" "node_exporter_full_dashboard" {
   config_json = file("${path.module}/1860_rev41.json")
 }
